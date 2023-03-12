dce87cba52c6255eecc6620a16dde97159e89482
Gazmend Nikqi <gazmendn93@gmail.com>
2023-03-12 13:22:40 +0100

    Update profile

---
+++ src/scripts/eventHandlers/updateProfile.mjs
@@ -0,0 +1,37 @@
+import { updateProfile } from "../api/profile/index.mjs";
+import { readProfile } from "../api/profile/index.mjs";
+import { load } from "../localStorage.mjs";
+
+export async function setUpdateProfileFormListener(){
+    const form = document.querySelector("#editProfile");
+
+    
+    
+    if (form){
+        //Get name and email from local storage
+        const { name, email } = load("profile");
+        form.name.value = name;
+        form.email.value = email;
+
+        
+        const profile = await readProfile(name);
+
+        
+        form.banner.value = profile.banner;
+        form.avatar.value = profile.avatar;
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const formData = new FormData(form);
+        const profile = Object.fromEntries(formData.entries());
+        
+        profile.name = name;
+        profile.email = email;
+        
+        updateProfile(profile);
+        });
+
+    };
+};
+
