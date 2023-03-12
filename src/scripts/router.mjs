b01ef1851d6cefa7e6eaaf3af42e4f04e46851da
Gazmend Nikqi <gazmendn93@gmail.com>
2023-03-12 13:21:51 +0100

    Created path router code

---
+++ src/scripts/router.mjs
@@ -0,0 +1,24 @@
+import * as listeners from "./eventHandlers/index.mjs";
+
+
+
+const path = location.pathname;
+
+switch (path){
+    case '/login.html':
+        listeners.loginEventListener()
+        break;
+    case '/register.html':
+        listeners.setRegisterFormListener()
+        break;
+    case '/post/create.html/':
+        listeners.createPostEventListener()
+        break;
+    case '/post/edit/':
+        listeners.updatePostEventListener()
+        break;
+    case '/profile/edit/':
+        listeners.setUpdateProfileFormListener()
+        break;
+};
+
